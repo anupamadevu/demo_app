989fef13372c16688ea7051a72fd9c87da35942f
anupamadevu <anupamadevu004@gmail.com>
2025-07-16 15:00:40 +0530

    initial commit to branch develop

--- src/App.jsx
+++ src/App.jsx
@@ -18,9 +18,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <h2>This is develop  branch </h2>
-      <h3>This is branch one</h3>
-      <h2>This is branch develop</h2>
-      <h2> This is branch 3</h2>
+      
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
