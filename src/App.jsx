de89a1fb6137a483db9c4327afb29f82591b32ac
anupamadevu <anupamadevu004@gmail.com>
2025-07-16 15:44:09 +0530

    checking changes in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -17,8 +17,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <h2>This is develop  branch </h2>
-      
+      <h2>This is develop  branch </h2>     
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
